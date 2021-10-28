3ca580cebcdd0870ac0b22f1abebd6f99df68e8a
jianghh <jianghh@2345.com>
2021-10-28 09:50:45 +0800

    fix: mac拉取远程仓库报错

--- demo/download-repo.js
+++ demo/download-repo.js
@@ -6,7 +6,6 @@ const download = require("download-git-repo");
 download(
     "github:365fed/admin-temp#main", 
     'app',
-    { clone: true },
     function (err) {
       console.log(err ? "Error" : "Success", err);
     }
